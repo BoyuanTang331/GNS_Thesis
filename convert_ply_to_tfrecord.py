39d65c32505e18f29b19fce95bad89902ebd4136
BoyuanTang331 <tangboyuan01@gmail.com>
2023-09-23 02:50:26 +0200

    update after debug

--- convert_ply_to_tfrecord.py
+++ convert_ply_to_tfrecord.py
@@ -10,7 +10,7 @@ import os
 def read_ply_file(file_path):
     ply_data = PlyData.read(file_path)
     vertex_data = ply_data['vertex']
-    points = np.column_stack([vertex_data['x'], vertex_data['y'], vertex_data['z']])
+    points = np.column_stack([vertex_data['x'], vertex_data['y']]) #vertex_data['z']])
     points = points[:, :, np.newaxis]
     return points
 
@@ -20,21 +20,21 @@ def numpy_array_to_example(particle_type, key, points):
         feature={
             'particle_type':tf.train.Feature(int64_list=tf.train.Int64List(value=particle_type)),
             'key':tf.train.Feature(int64_list=tf.train.Int64List(value=[key])),
-            'points': tf.train.Feature(float_list=tf.train.FloatList(value=points.flatten))
+            'points': tf.train.Feature(float_list=tf.train.FloatList(value=points.flatten()))
         }))
     return example
 
 
-def save_to_tfrecord(particle_type,keys,points, filename):
+def save_to_tfrecord(particle_type,keys,data, filename):
     with tf.io.TFRecordWriter(filename) as writer:
-        for i in range(len(points)):
-            example = numpy_array_to_example(particle_type,key[i],point[i])
+        for i in range(len(data)):
+            example = numpy_array_to_example(particle_type,keys[i],data[i])
             writer.write(example.SerializeToString())
 
 
-data_dir = r'C:\Users\84017\Desktop\MPM_dataset'
+data_dir = r'C:\Users\84017\Desktop\mpm'
 data_files = os.listdir(data_dir)
-data = np.empty((4000, 3, 1),dtype=np.float32)
+data = np.empty((2000, 4, 1),dtype=np.float32)
 particle_type = np.full((data.shape[0],), 5, dtype=np.int64)
 keys = []
 
@@ -51,9 +51,13 @@ keys = []
 
 for i, f in enumerate(data_files):
     points = read_ply_file(os.path.join(data_dir, f))
-    data = np.concatenate([data, points], axis=2)
+    data = np.concatenate([data, points], axis=1) #if 2D axis=1 if 3D axis=2
     keys.append(i)
 
-data = data[:, :, 1:]
-save_path = r'C:\Users\84017\Desktop\MPM_dataset\Water2D_small.tfrecord'
+data = data[:, :, 2:]
+save_path = r'C:\Users\84017\Desktop\mpm\Water2D_small.tfrecord'
 save_to_tfrecord(particle_type, keys, data, save_path)
+
+
+print("data_files:", data_files)
+print("keys:", keys)
